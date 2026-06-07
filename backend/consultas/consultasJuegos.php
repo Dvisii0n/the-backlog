e2a0ed0930e644f43897ae7b87c831296e78d17d
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-06-06 21:07:01 -0600

    se arreglo consulta que permitia al usuario ver todos los juegos en vez de los suyos

--- backend/consultas/consultasJuegos.php
+++ backend/consultas/consultasJuegos.php
@@ -1,8 +1,11 @@
 <?php
 
-function get_juegos($conn)
+function get_juegos($conn, $id_propietario)
 {
-    $resultado = $conn->query('SELECT * FROM juegos');
+    $stmt = $conn->prepare('SELECT * FROM juegos WHERE id_propietario=?');
+    $stmt->bind_param('i', $id_propietario);
+    $stmt->execute();
+    $resultado = $stmt->get_result();
     $datos = $resultado->fetch_all(MYSQLI_ASSOC);
     return $datos;
 }
@@ -18,10 +21,10 @@ function get_juego($conn, $id)
 }
 
 
-function crear_juego($conn, $datos_juego)
+function crear_juego($conn, $datos_juego, $id_propietario)
 {
     $stmt = $conn->prepare('INSERT INTO juegos (nombre, genero, estado, portada_url, hrs_finalizacion, fecha_lanzamiento, clasificacion, steam_url, calificacion_igdb, calificacion_personal, id_propietario) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)');
-    $stmt->bind_param('ssssdsssiii', $datos_juego['nombre'], $datos_juego['genero'], $datos_juego['estado'], $datos_juego['portada_url'], $datos_juego['hrs_finalizacion'], $datos_juego['fecha_lanzamiento'], $datos_juego['clasificacion'], $datos_juego['steam_url'], $datos_juego['calificacion_igdb'], $datos_juego['calificacion_personal'], $datos_juego['id_propietario']);
+    $stmt->bind_param('ssssdsssiii', $datos_juego['nombre'], $datos_juego['genero'], $datos_juego['estado'], $datos_juego['portada_url'], $datos_juego['hrs_finalizacion'], $datos_juego['fecha_lanzamiento'], $datos_juego['clasificacion'], $datos_juego['steam_url'], $datos_juego['calificacion_igdb'], $datos_juego['calificacion_personal'], $id_propietario);
     $stmt->execute();
     $conn->commit();
     return;
