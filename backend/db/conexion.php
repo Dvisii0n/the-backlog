6f70e6d9b6257b5f07182a48ca369c60a8f54085
Dvisii0n <191772908+Dvisii0n@users.noreply.github.com>
2026-06-06 21:08:10 -0600

    se reemplazo carga directa de .env por dotenvLoader.php

--- backend/db/conexion.php
+++ backend/db/conexion.php
@@ -1,10 +1,6 @@
 <?php
 
-require __DIR__ . './../vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__, '../.env');
-$dotenv->load();
-
+include '../dotenvLoader.php';
 $conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 
 if ($conn->connect_error) {
